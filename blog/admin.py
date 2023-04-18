02bb97de53fa0c7742bdb1a64677c9416b7486aa
Luis Gustavo <piaizete123@gmail.com>
2023-04-17 23:02:02 -0300

    generate unique slug with title+id

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     active = models.BooleanField(default=True)
 
@@ -17,3 +19,9 @@ class Article(models.Model):
 
     def get_absolute_url_delete(self):
         return reverse("article-delete", kwargs={"id": self.id})
+
+    def save(self, *args, **kwargs):
+        super(Article, self).save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.title) + f'-{self.id}'
+            self.save()
