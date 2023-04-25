7e272a8b13722c87aa1b1e05465f1a1613d83885
Luis Gustavo <piaizete123@gmail.com>
2023-04-24 22:10:31 -0300

    replaced id to slug

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -9,12 +10,19 @@ class Recipe(models.Model):
     description = models.TextField(null=False, blank=False)
     created_at = models.DateField(auto_now_add=True)
     active = models.BooleanField(default=True)
+    slug = models.SlugField(blank=True, null=True)
 
     def get_absolute_url(self):
-        return reverse("recipes-detail", kwargs={"id": self.id})
+        return reverse("recipes-detail", kwargs={"slug": self.slug})
 
     def get_absolute_url_edit(self):
-        return reverse("recipes-edit", kwargs={"id": self.id})
+        return reverse("recipes-edit", kwargs={"slug": self.slug})
 
     def get_absolute_url_delete(self):
-        return reverse("recipes-delete", kwargs={"id": self.id})
+        return reverse("recipes-delete", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        super(Recipe, self).save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.title) + f'-{self.id}'
+            self.save()
--- recipes/views.py
+++ recipes/views.py
@@ -14,8 +14,8 @@ def recipes_list_view(request):
     return render(request, 'recipes/recipe_list.html', context)
 
 
-def recipes_detail_view(request, id):
-    obj = get_object_or_404(Recipe, id=id)
+def recipes_detail_view(request, slug):
+    obj = get_object_or_404(Recipe, slug=slug)
     context = {'obj': obj}
     return render(request, 'recipes/recipe_detail.html', context)
 
@@ -31,8 +31,8 @@ def recipes_create_view(request):
 
 
 @login_required
-def recipes_edit_view(request, id):
-    obj = get_object_or_404(Recipe, id=id)
+def recipes_edit_view(request, slug):
+    obj = get_object_or_404(Recipe, slug=slug)
     form = RecipeForm(request.POST or None, instance=obj)
     if form.is_valid():
         saved_obj = form.save()
@@ -44,8 +44,8 @@ def recipes_edit_view(request, id):
 
 
 @login_required
-def recipes_delete_view(request, id):
-    obj = get_object_or_404(Recipe, id=id)
+def recipes_delete_view(request, slug):
+    obj = get_object_or_404(Recipe, slug=slug)
     if request.method == 'POST':
         obj.delete()
         return redirect('../../')
