83ecaf3c1e8e894ba07c263e663af0a15847bfdd
Luis Gustavo <piaizete123@gmail.com>
2023-04-25 12:06:17 -0300

    admin search field improved

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Recipe
 
+
 # Register your models here.
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title']
+    search_fields = ['user__username', 'title', 'description']
+
+
+admin.site.register(Recipe, RecipeAdmin)
