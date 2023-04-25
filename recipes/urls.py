94dd05903f5b35634df6a2303ea6e7ceef106e91
Luis Gustavo <piaizete123@gmail.com>
2023-04-24 21:06:54 -0300

    recipes delete view created

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import recipes_create_view, recipes_detail_view, recipes_list_view
+from .views import (recipes_create_view, recipes_detail_view,
+                    recipes_edit_view, recipes_list_view)
 
 urlpatterns = [
     path('', recipes_list_view, name='recipes-list'),
-    path('<int:id>/', recipes_detail_view, name='recipes-detail'),
     path('create/', recipes_create_view, name='recipes-create'),
+    path('<int:id>/', recipes_detail_view, name='recipes-detail'),
+    path('<int:id>/edit/', recipes_edit_view, name='recipes-edit'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -10,13 +10,13 @@ from .models import Recipe
 def recipes_list_view(request):
     obj = Recipe.objects.all()
     context = {'objects': obj}
-    return render(request, 'recipes/list.html', context)
+    return render(request, 'recipes/recipe_list.html', context)
 
 
 def recipes_detail_view(request, id):
     obj = get_object_or_404(Recipe, id=id)
     context = {'obj': obj}
-    return render(request, 'recipes/detail.html', context)
+    return render(request, 'recipes/recipe_detail.html', context)
 
 
 @login_required
@@ -24,6 +24,19 @@ def recipes_create_view(request):
     form = RecipeForm(request.POST or None)
     if form.is_valid():
         obj = form.save()
-        return redirect(obj)  # arrumar isso
+        return redirect(obj)
     context = {'form': form}
-    return render(request, 'recipes/recipes_create.html', context)
+    return render(request, 'recipes/recipe_create.html', context)
+
+
+@login_required
+def recipes_edit_view(request, id):
+    obj = get_object_or_404(Recipe, id=id)
+    form = RecipeForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        saved_obj = form.save()
+        return redirect(saved_obj)
+    context = {
+        'form': form
+    }
+    return render(request, 'recipes/recipe_edit.html', context)
